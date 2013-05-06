b1e8a142d21499ba59edf8a7e220f9a820243b3b
black apple <wenzhou.1991@163.com>
2013-05-06 09:30:02 +0800

    Initial Commit

---
+++ Classmate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Classmate
+//
+//  Created by icreative-mini on 13-5-6.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Classmate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Classmate
+//
+//  Created by icreative-mini on 13-5-6.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
